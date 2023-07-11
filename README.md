ab8bf53f5307c4c364c3588cc4bb315aec6b7805
RawadKadi <rawadkady@gmail.com>
2023-07-11 13:58:11 +0100

    Finalizing project

---
+++ README.md
@@ -0,0 +1,58 @@
+# Task Management API
+
+This repository contains the source code for a Task Management API, built using Flask and PostgreSQL.
+
+## Prerequisites
+
+Before running the application, make sure you have the following prerequisites installed on your machine:
+
+- Python (version 3.11.0)
+- PostgreSQL (version 11 or higher) | Supported Versions: Current (15) / 14 / 13 / 12 / 11
+
+
+## Getting Started
+
+To get started with the Task Management API, follow these steps:
+
+1. Clone the repository to your local machine.
+2. Navigate to the project directory.
+3. Install the project dependencies using pip.
+4. Set up the PostgreSQL database by creating a new database named `task_manager` and updating the database connection URI in the `app.py` file with your PostgreSQL credentials.
+5. Apply the database migrations.
+6. Launch the application.
+
+The Task Management API will be accessible at http://localhost:5000.
+
+## API Endpoints
+
+The Task Management API provides the following endpoints:
+
+- GET /tasks: Retrieve a list of all tasks.
+- GET /tasks/{task_id}: Retrieve details of a specific task.
+- POST /tasks: Create a new task.
+- PUT /tasks/{task_id}: Update the details of a specific task.
+- DELETE /tasks/{task_id}: Delete a specific task.
+
+For detailed information on the request and response formats for each endpoint, please refer to the API documentation.
+
+## Swagger Documentation
+
+The Task Management API documentation is provided using Swagger UI. To access the Swagger documentation, follow these steps:
+
+1. Launch the application.
+2. Open your web browser and go to http://localhost:5000/swagger.
+
+The Swagger UI interface will allow you to explore and interact with the API endpoints in a user-friendly way.
+
+## Contributing
+
+Contributions are welcome! If you find any issues or have suggestions for improvements, please create an issue or submit a pull request.
+
+## License
+
+This project is licensed under the MIT License.
+
+## Acknowledgements
+
+- Flask: Lightweight web framework for Python.
+- PostgreSQL: Powerful open-source relational database management system.
---
+++ app.py
@@ -0,0 +1,126 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_restx import Api, Resource
+from flask_swagger_ui import get_swaggerui_blueprint
+
+
+app = Flask(__name__)
+api = Api(app, version='1.0', title='Task Management API', description='API endpoints for managing tasks')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:rawad.18@localhost:5432/task_manager'
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+SWAGGER_URL = '/swagger'
+API_URL = '/swagger.json'
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Task Management API"
+    }
+)
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    description = db.Column(db.Text)
+    completed = db.Column(db.Boolean, default=False)
+    priority = db.Column(db.String(50), default='low')
+    due_date = db.Column(db.Date)
+    category = db.Column(db.String(50))
+
+    def __init__(self, title, description, priority,due_date,category):
+        self.title = title
+        self.description = description
+        self.priority = priority
+        self.due_date = due_date
+        self.category = category
+
+
+@api.route('/tasks')
+class TasksResource(Resource):
+    def get(self):
+        tasks = Task.query.all()
+        result = []
+        for task in tasks:
+            result.append({
+                'id': task.id,
+                'title': task.title,
+                'description': task.description,
+                'completed': task.completed,
+                'priority': task.priority,
+                'due_date': task.due_date,
+                'category': task.category
+            })
+        return jsonify(result)
+
+    def post(self):
+        data = request.get_json()
+        title = data.get('title')
+        description = data.get('description')
+        priority = data.get('priority')
+        due_date = data.get('due_date')
+        category = data.get('category')
+
+        task = Task(title=title, description=description, priority=priority, due_date=due_date, category=category)
+        db.session.add(task)
+        db.session.commit()
+        return jsonify({'message': 'Task created successfully'})
+
+
+@api.route('/tasks/<int:task_id>')
+class TaskResource(Resource):
+    def get(self, task_id):
+        task = Task.query.get(task_id)
+        if task:
+            result = {
+                'id': task.id,
+                'title': task.title,
+                'description': task.description,
+                'completed': task.completed,
+                'priority': task.priority,
+                'due_date': task.due_date,
+                'category': task.category
+            }
+            return jsonify(result)
+        else:
+            return jsonify({'message': 'Task not found'}), 404
+
+    def put(self, task_id):
+        task = Task.query.get(task_id)
+        if not task:
+            return jsonify({'error': 'Task not found'})
+
+        data = request.get_json()
+        if 'title' in data:
+            task.title = data['title']
+        if 'description' in data:
+            task.description = data['description']
+        if 'completed' in data:
+            task.completed = data['completed']
+        if 'priority' in data:
+            task.priority = data['priority']
+        if 'due_date' in data:
+            task.due_date = data['due_date']
+        if 'category' in data:
+            task.category = data['category']
+
+        db.session.commit()
+        return jsonify({'message': 'Task updated successfully'})
+
+    def delete(self, task_id):
+        task = Task.query.get(task_id)
+        if not task:
+            return jsonify({'message': 'Task not found'}), 404
+
+        db.session.delete(task)
+        db.session.commit()
+
+        return jsonify({'message': 'Task deleted successfully'}), 200
+if __name__ == '__main__':
+    app.run(debug=True)
